d1acc6c6695e66fc45f771e9e04c1f04b6a7256b
krishna rokhale <krishnarokhale@krishnas-iMac.local>
2017-09-21 20:41:07 +0530

    styling

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,47 +4,32 @@ import './css/homepage.css';
 
 export default function Home(props) {
     return (
-        <div className="home-page">
-            <section className="col-md-12 upper-section bor">
-                <div className="col-md-12 bor">
-                    <h1 className="text-calm-blue text-vert-middle">Use daily meditation to improve your life!</h1>
+        <div className="home-page ">            
+            <section className="col-md-12 upper-section man pan">
+                <div className="col-md-12 man pan">
+                    <h1 className="spaced-out mtl ptl text-white text-vert-middle mtsl">Use daily meditation to improve your life!</h1>
                 </div>
-                <div>
-                    <button className="">Create account</button>
-                </div>
+                
 
             </section>
             <section className="col-md-12">
-                <div className="col-md-12">
-                    <h2 className="text-calm-blue">Proven Benefits of Meditation</h2>
-                </div>
+                
 
                 <div className="info-columns">
-                    <div className="col-lg-4 col-md-4 col-sm -12 left-info">
-                        <p>place holder</p>
+                    <div className="col-lg-4 text-center  col-md-4 col-sm -12 left-info text-white">
+                        <h4>Proven Benefits of Meditation</h4>
+                        <p>Proven Benefits of Meditation</p>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm -12 middle-info">
-                        <p>place holder</p>
+                    <div className="col-lg-4 text-center  col-md-4 col-sm -12 left-info text-white">
+                        <h4>Proven Benefits of Meditation</h4>
+                        <p>Proven Benefits of Meditation</p>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm -12 right-info">
-                        <p>place holder</p>
-                    </div>
-                </div>
-            </section>
-            <section className="fixed-bg">
-                {/*put image here*/}
-            </section>
-            <section className="description-section">
-                <h2>How it works</h2>
-                <div className="description-columns">
-                    <div className="col-lg-6 col-md-6 col-sm-12 left-column">
-                        <p>placeholder</p>
-                    </div>
-                    <div className="col-lg-6 col-md-6 col-sm-12 right-column">
-                        <p>placeholder</p>
+                    <div className="col-lg-4 text-center  col-md-4 col-sm -12 left-info text-white">
+                        <h4>Proven Benefits of Meditation</h4>
+                        <p>Proven Benefits of Meditation</p>
                     </div>
                 </div>
-            </section>
+            </section>            
         </div>
     )
 }
--- src/components/landing.js
+++ src/components/landing.js
@@ -13,36 +13,49 @@ export default class Landing extends Component{
 
     render(){
 
+        const calmPageStyle = {"min-height": "1500px"}
+
         return(
-            <div className="col-md-12 pal">
-                {/*description section*/}
-                <div className="col-md-12 bor">
-                    <h2 className="main-text text-center">
-                        How to Use
-                    </h2>
-                </div>
-                {/*instructions and screen shots*/}
-                <div className="col-md-12 pal">
-                    <div className="col-md-6 pam bor image-holder">
-                        image here
-                    </div>
-                    <div className="col-md-6 pam bor text-holder">
-                        text here
-                    </div>
-                </div>
-                <div className="col-md-12 pal">
-                    <div className="col-md-6 pam bor text-holder">
-                        text here
-                    </div>
-                    <div className="col-md-6 pam bor image-holder">
-                        image here
-                    </div>
-                </div>
+            <div className="col-md-12 pal bg-calm" style={calmPageStyle}>
+                
                 <div className="col-md-12">
-                    <div className="col-md-12 bor demo-account-info">
-                        demo account information
-                        <button className="demo-login-button" onClick={this.login}>Login</button>
+                    <h3 className="main-text text-center spaced-out text-white">
+                        How to Use
+                    </h3>
+                </div>
+                
+                <div className="col-md-12 pal">
+                    <div className="col-md-6 pam image-holder min-height-500 bor">
+                        <p className="text-white text-center">image here akjshd</p>
                     </div>
+                    <div className="col-md-6 pam text-holder pal">
+                        <p className="text-white text-center pal">Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        </p>
+                    </div>
+                </div>
+                <div className="col-md-12 pal">                
+                <div className="col-md-6 pam text-holder pal">
+                        <p className="text-white text-center pal">Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        Hurricane Maria was battering the Dominican Republic on Thursday as the long-term devastation in Puerto Rico was just coming into focus.
+                        </p>
+                    </div>
+                    
+                    <div className="col-md-6 pam image-holder min-height-500 bor">
+                        <p className="text-white text-center">image here akjshd</p>
+                    </div>
+                    
+
+                </div>
+                <div className="col-md-12">                
                 </div>
             </div>
         )
