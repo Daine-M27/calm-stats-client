3d86de46a52d0cda034c4ee189375e06e67c440e
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-09-25 22:36:14 -0400

    travis

--- src/components/dailyChart.js
+++ src/components/dailyChart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, CartesianAxis, Tooltip, Legend} from 'recharts';
-import ResponsiveContainer from "recharts/es6/component/ResponsiveContainer";
+// import ResponsiveContainer from "recharts/es6/component/ResponsiveContainer";
 const demoData = [
     {date: 'Sep 1', time: 23},
     {date: 'Sep 2', time: 18},
@@ -28,17 +28,21 @@ export default class DailyChart extends Component{
     render () {
         console.log(this.props.data);
         return (
-            <ResponsiveContainer height='100%' width='100%'>
-                <LineChart data={this.props.data}
-                           margin={{top: 20, right: 50, left: 0, bottom: 10}}>
-                    <XAxis dataKey="date"/>
-                    <YAxis/>
-                    <CartesianGrid stroke="yellow" strokeDasharray="3 3" />
-                    <CartesianAxis stroke="yellow" />
-                    <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
-                    <Line type="monotone" dataKey="time" stroke="yellow" fill="#e9e3a7"/>
-                </LineChart>
-            </ResponsiveContainer>
+           // <ResponsiveContainer height='100%' width='100%'>
+
+               <LineChart height={200} width={800} data={this.props.data}
+                          margin={{top: 20, right: 50, left: 0, bottom: 10}}>
+                   <XAxis dataKey="date"/>
+                   <YAxis/>
+                   <CartesianGrid stroke="yellow" strokeDasharray="3 3" />
+                   <CartesianAxis stroke="yellow" />
+                   <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+                   <Line type="monotone" dataKey="time" stroke="yellow" fill="#e9e3a7"/>
+               </LineChart>
+
+
+
+            // </ResponsiveContainer>
         );
     }
 }
