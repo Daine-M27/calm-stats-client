cf9ea95d05268f3d2b8031be4ec19e81a210e2a9
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-10-02 21:28:58 -0400

    updated url

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -20,7 +20,7 @@ export default class Auth {
     this.isAuthenticated = this.isAuthenticated.bind(this);
     //this.serverUrl = 'http://localhost:3001/api/v1/users/';
     //this.serverUrl = 'https://gentle-sea-29060.herokuapp.com/api/v1/users/';
-      this.serverUrl = 'https://calmstats.com/api/v1/users/'
+    this.serverUrl = 'https://calmstats.com/api/v1/users/';
   }
 
 
